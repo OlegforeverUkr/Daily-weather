1ef4ce4dc025eb21dd1378ce4742771e0a305b9b
Oleg <shlyapin8848@gmail.com>
2024-08-08 15:01:45 +0300

    added docstrings and type annotation for functions

--- app.py
+++ app.py
@@ -13,35 +13,36 @@ TELEGRAM_BOT_TOKEN = os.getenv("TOKEN")
 TELEGRAM_CHANNEL_ID = os.getenv('TELEGRAM_CHANNEL_ID')
 
 
-def get_weather():
+def get_weather() -> str:
+    # Create a cache session with an expiration time of 1 hour
     cache_session = requests_cache.CachedSession('.cache', expire_after=3600)
+    # Create a retry session with 5 retries and a backoff factor of 0.2
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+    # Create an OpenMeteo client with the retry session
     openmeteo = openmeteo_requests.Client(session=retry_session)
 
     url = "https://api.open-meteo.com/v1/forecast"
     params = {
-        "latitude": 49.4183,
-        "longitude": 26.9794,
+        "latitude": 49.453551,
+        "longitude": 27.014202,
         "hourly": ["temperature_2m", "apparent_temperature"],
         "timezone": None,
         "forecast_days": 1
     }
 
+    # Retrieve the weather forecast
     responses = openmeteo.weather_api(url, params=params)
     response = responses[0]
-
-    print(f"Coordinates {response.Latitude()}°N {response.Longitude()}°E")
-    print(f"Elevation {response.Elevation()} m asl")
-    print(f"Timezone {response.Timezone()} {response.TimezoneAbbreviation()}")
-    print(f"Timezone difference to GMT+0 {response.UtcOffsetSeconds()} s")
 
     hourly = response.Hourly()
     hourly_temperature_2m = hourly.Variables(0).ValuesAsNumpy()
     hourly_apparent_temperature = hourly.Variables(1).ValuesAsNumpy()
 
+    # Round the temperature values to 1 decimal place
     normal_temp_2m = [round(float(i), 1) for i in hourly_temperature_2m]
     normal_apparent_temperature = [round(float(i), 1) for i in hourly_apparent_temperature]
 
+    # Create a date range for the hourly forecast
     hourly_time = pd.date_range(
         start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
         end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
@@ -50,30 +51,36 @@ def get_weather():
     )
     hourly_hours_only = hourly_time.strftime("%H:00")
 
+    # Create a DataFrame with the forecast data
     hourly_data = {
         "Часы": hourly_hours_only,
         "Температура": normal_temp_2m,
         "Ощущается как": normal_apparent_temperature
     }
-
     hourly_dataframe = pd.DataFrame(data=hourly_data)
+
+    # Return a string representation of the DataFrame
     return hourly_dataframe.to_string()
 
 
-
-def send_to_telegram(message):
+def send_to_telegram(message: str) -> dict:
+    # Construct the URL for the Telegram Bot API
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
 
+    # Set the parameters for the request
     params = {
         'chat_id': TELEGRAM_CHANNEL_ID,
         'text': f'Доброе утро!\nПогода на сегодня\n\n{message}'
     }
 
+    # Send the request to the Telegram Bot API
     res = requests.post(url, params=params)
+
+    # Raise an exception if the request fails
     res.raise_for_status()
 
+    # Return the JSON response from the Telegram Bot API
     return res.json()
-
 
 
 if __name__ == '__main__':
